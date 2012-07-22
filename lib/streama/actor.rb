f97fa5ba3a412002ea6647729fe9bc3820133254
Holger Sindbaek <holgersindbaek@gmail.com>
2012-07-22 15:31:18 -0700

    fixed the limit

--- lib/streama/actor.rb
+++ lib/streama/actor.rb
@@ -54,12 +54,12 @@ module Streama
         incoming_activity options
       end
 
-      def incoming_activity(options = {}, limit = {})
-        activity_class.stream_for(self, options).limit(limit)
+      def incoming_activity(options = {})
+        activity_class.stream_for(self, options).limit(30)
       end
 
       def outgoing_activity(options = {})
-        activity_class.stream_of(self, options).limit(10)
+        activity_class.stream_of(self, options).limit(30)
       end
 
       def activity_class
